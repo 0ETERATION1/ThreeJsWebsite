738771a7c555a89d42965e83324fc4c90dac436e
Alex Terskin <sasha@Alexs-MJOLNIR.local>
2024-06-05 15:52:16 -0400

    add a logo"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import { BrowserRouter } from "react-router-dom";
 
+import { About, Contact, Experience, Feedbacks, Hero,
+  Navbar, Tech, Works, StarsCanvas } from './components';
 
 const App = () => {
 
   return (
-    <div>
-      My 3d website
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat
+        bg-center">
+          <Navbar />
+          <Hero />
+
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+    // <div>
+    //   My 3d website
+    // </div>
   )
 }
 
